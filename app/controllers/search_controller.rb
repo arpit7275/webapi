0cc2aeb235e37773b5d5642308348b18f2b078c1
arpit7275 <arpit.sethi2@live.com>
2015-06-15 08:24:07 +0530

    Added URL encoding

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,7 +6,9 @@ class SearchController < ApplicationController
         @artistname = params[:artist] 
         @url2 = "&api_key=161861de736d0ac2273fda645becb5a8&format=json"
         @url = @url1 + @artistname + @url2
+        @url = URI::encode(@url)
         @url_similar = @urlsimi + @artistname + @url2
+        @url_similar = URI::encode(@url_similar)
         require 'open-uri'
         @artist_det = JSON.parse(open(@url).read)
         @similar_artist_det = JSON.parse(open(@url_similar).read)
