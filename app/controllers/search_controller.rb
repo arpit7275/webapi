a6cc5017d6f4610614126e442e666b717483a857
arpit7275 <arpit.sethi2@live.com>
2015-06-15 06:54:03 +0530

    last commit

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,14 @@
+class SearchController < ApplicationController
+    def artist
+        @url1 = "http://ws.audioscrobbler.com/2.0/?method=artist.search&artist="
+        
+        @urlsimi = "http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist="
+        @artistname = params[:artist] 
+        @url2 = "&api_key=161861de736d0ac2273fda645becb5a8&format=json"
+        @url = @url1 + @artistname + @url2
+        @url_similar = @urlsimi + @artistname + @url2
+        require 'open-uri'
+        @artist_det = JSON.parse(open(@url).read)
+        @similar_artist_det = JSON.parse(open(@url_similar).read)
+    end
+end
